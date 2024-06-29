700ef7ec59aecd1d4b8e860269c3cd605f1687fb
Toxic Butterfly <qwerty541683@gmail.com>
2024-06-29 13:27:00 +0300

    init commit

---
+++ src/test/java/org/example/exchangeratesservice/ExchangeRatesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.exchangeratesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRatesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
