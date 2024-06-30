d9dacd6436101e92af551dbe44847cfd2ed2de4e
Toxic Butterfly <qwerty541683@gmail.com>
2024-06-30 17:18:47 +0300

    Add exceptions

--- src/main/java/org/example/exchangeratesservice/controller/CurrencyRateController.java
+++ src/main/java/org/example/exchangeratesservice/controller/CurrencyRateController.java
@@ -1,5 +1,8 @@
 package org.example.exchangeratesservice.controller;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.example.exchangeratesservice.model.CurrencyRate;
@@ -7,6 +10,7 @@ import org.example.exchangeratesservice.service.CurrencyRateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,18 +21,21 @@ import java.time.LocalDate;
 @RestController
 @RequestMapping("api/v1")
 @RequiredArgsConstructor
+@Validated
 public class CurrencyRateController {
 
     private final CurrencyRateService currencyRateService;
 
+    // Pre download exchange rates for all currencies on a specific day if possible and return if download was successful
     @GetMapping("load-exrates")
-    public ResponseEntity<String> loadCurrencyRatesByDate(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+    public ResponseEntity<String> loadCurrencyRatesByDate(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @NotNull @PastOrPresent LocalDate date) {
         return currencyRateService.loadCurrencyRatesByDate(date);
     }
 
+    // Returns the exchange rate to a specific currency on a specific day
     @GetMapping("get-exrate")
-    public ResponseEntity<CurrencyRate> getCurrencyRate(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
-                                                        Integer code) {
+    public ResponseEntity<CurrencyRate> getCurrencyRate(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @NotNull @PastOrPresent LocalDate date,
+                                                        @NotNull(message = "Код валюты не введён") @Min(value = 1, message = "Код валюты должен быть не меньше 1") Integer code) {
         return ResponseEntity.ok(currencyRateService.getCurrencyRate(date, code));
     }
 }
--- src/main/java/org/example/exchangeratesservice/service/impl/CurrencyRateServiceImpl.java
+++ src/main/java/org/example/exchangeratesservice/service/impl/CurrencyRateServiceImpl.java
@@ -1,8 +1,10 @@
 package org.example.exchangeratesservice.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.example.exchangeratesservice.converter.CurrencyDTOConverter;
 import org.example.exchangeratesservice.dto.CurrencyDTO;
+import org.example.exchangeratesservice.exception.CurrencyExchangeNotFoundException;
 import org.example.exchangeratesservice.model.CurrencyRate;
 import org.example.exchangeratesservice.repo.CurrencyRateRepo;
 import org.example.exchangeratesservice.service.CurrencyRateService;
@@ -12,7 +14,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.Optional;
 
 @Service
@@ -22,25 +23,24 @@ public class CurrencyRateServiceImpl implements CurrencyRateService {
     private final CurrencyRateRepo currencyRateRepo;
     private final CurrencyDTOConverter converter;
 
+    // Pre download exchange rates for all currencies on a specific day if possible and return if download was successful
     @Override
     public ResponseEntity<String> loadCurrencyRatesByDate(LocalDate date) {
         RestTemplate restTemplate = new RestTemplate();
         String url = "https://api.nbrb.by/exrates/rates?ondate=" + date + "&periodicity=0";
-        System.out.println(url);
-        try {
-            CurrencyDTO[] rates = restTemplate.getForObject(url, CurrencyDTO[].class);
-            System.out.println(Arrays.toString(restTemplate.getForObject(url, CurrencyDTO[].class)));
-            assert rates != null;
-            for (CurrencyDTO rate : rates) {
-                System.out.println(rate);
-                currencyRateRepo.save(converter.convertCurrencyDTOToCurrencyRate(rate));
-            }
-            return ResponseEntity.ok("Data for " + date.toString() + " loaded successfully.");
-        } catch (Exception e) {
+
+        CurrencyDTO[] rates = restTemplate.getForObject(url, CurrencyDTO[].class);
+        if (rates == null || rates.length == 0 || !rates[0].getDate().toLocalDate().equals(date)) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to load data for " + date.toString());
         }
+        for (CurrencyDTO rate : rates) {
+            currencyRateRepo.save(converter.convertCurrencyDTOToCurrencyRate(rate));
+        }
+        return ResponseEntity.ok("Data for " + date + " loaded successfully.");
     }
 
+    // Returns the exchange rate to a specific currency on a specific day
+    @SneakyThrows
     @Override
     public CurrencyRate getCurrencyRate(LocalDate date, Integer code) {
         Optional<CurrencyRate> exrate = currencyRateRepo.findCurrencyRateByDateAndCurId(date, code);
@@ -52,7 +52,9 @@ public class CurrencyRateServiceImpl implements CurrencyRateService {
             System.out.println(url);
             RestTemplate restTemplate = new RestTemplate();
             CurrencyDTO rate = restTemplate.getForObject(url, CurrencyDTO.class);
-            System.out.println(rate);
+            if (rate == null || !rate.getDate().toLocalDate().equals(date)) {
+                throw new CurrencyExchangeNotFoundException("Failed to load currency exchange");
+            }
             CurrencyRate exchangeRate = converter.convertCurrencyDTOToCurrencyRate(rate);
             currencyRateRepo.save(exchangeRate);
             return exchangeRate;
