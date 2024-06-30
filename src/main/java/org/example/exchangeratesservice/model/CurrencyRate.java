093b3ea769552457a1243c1ea82ec54ed4f90e65
Toxic Butterfly <qwerty541683@gmail.com>
2024-06-30 18:29:44 +0300

    Add controller test

--- src/main/java/org/example/exchangeratesservice/model/CurrencyRate.java
+++ src/main/java/org/example/exchangeratesservice/model/CurrencyRate.java
@@ -4,12 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CurrencyRate {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
